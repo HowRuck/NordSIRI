fd27accb7e999aa34c09063162e5f07f0d6339f2
HowRuck <ertus123123@gmail.com>
2026-03-12 13:46:50 +0100

    feat(admin): Add Spring Boot Admin configuration and dependencies
    Integrate Spring Boot Admin for better application monitoring and management.

--- src/main/java/org/example/sirianalyzer/SiriAnalyzerApplication.java
+++ src/main/java/org/example/sirianalyzer/SiriAnalyzerApplication.java
@@ -1,11 +1,13 @@
 package org.example.sirianalyzer;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAdminServer
 public class SiriAnalyzerApplication {
 
     static void main(String[] args) {
