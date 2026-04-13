5cedbfe9202e9c4160afa728ff2354e0fcb02c65
HowRuck <ertus123123@gmail.com>
2026-04-13 17:40:54 +0200

    chore(run.sh): add ZGC and memory settings

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Dserver.port=8888 -Dspring.aot.enabled=true -jar target/SiriAnalyzer-0.0.1-SNAPSHOT.jar
+java -XX:+UseZGC -XX:+AlwaysPreTouch -Xms512m -Dserver.port=8888 -Dspring.aot.enabled=true -jar target/SiriAnalyzer-0.0.1-SNAPSHOT.jar
