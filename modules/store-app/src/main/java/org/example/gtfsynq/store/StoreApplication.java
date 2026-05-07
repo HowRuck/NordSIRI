4c5de0d64e05edff5af3e170025af0f6bd74e354
HowRuck <ertus123123@gmail.com>
2026-05-07 15:16:37 +0200

    chore(store): enable configuration properties

--- modules/store-app/src/main/java/org/example/gtfsynq/store/StoreApplication.java
+++ modules/store-app/src/main/java/org/example/gtfsynq/store/StoreApplication.java
@@ -3,6 +3,7 @@ package org.example.gtfsynq.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableKafkaStreams
 @ConfigurationPropertiesScan
+@EnableConfigurationProperties
 public class StoreApplication {
 
     public static void main(String[] args) {
